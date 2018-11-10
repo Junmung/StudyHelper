713558d73dde0d4238bd25fa390633e2975e0c49
Junmung <9174844@naver.com>
2018-11-10 20:06:47 +0900

    MemoDAO 추가

---
+++ app/src/main/java/com/example/junmung/StudyHelper/data/MemoDAO.java
@@ -0,0 +1,34 @@
+package com.example.junmung.StudyHelper.data;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface MemoDAO {
+    @Insert
+    void insert(Memo memo);
+
+
+    @Query("DELETE FROM memos WHERE _id IN (:ids) ")
+    void deleteCheckedMemos(int[] ids);
+
+    @Query("DELETE FROM memos WHERE _id = :id")
+    void delete(int id);
+
+    @Query("DELETE FROM memos")
+    void deleteAll();
+
+
+    @Query("SELECT * FROM memos ORDER BY registerDate DESC")
+    LiveData<List<Memo>> getAllMemos();
+
+    @Query("SELECT * FROM memos WHERE _id = :id")
+    LiveData<Memo> getMemoById(int id);
+
+
+
+}
